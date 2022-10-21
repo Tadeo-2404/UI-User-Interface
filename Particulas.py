b4c83e00fb8b412c7e2fb0715dd42e761ebcc27e
Tadeo Alvarez <tadeo.regalado.alvarez@gmail.com>
2022-10-21 14:32:42 -0500

    Corrigiendo constructor y funcion pow()

--- Particulas.py
+++ Particulas.py
@@ -1,16 +1,16 @@
 from algoritmos import distancia_euclidiana
 
 class Particula :
-    def __init__(self, id=0, origen_x=0, origen_y=0, destino_x=0, destino_y=0, velocidad=0, red = 0, green=0, blue=0, distancia=0.0):
-         self.__id  = 0
-         self.__origen_x = 0 
-         self.__origen_y = 0
-         self.__destino_x = 0
-         self.__destino_y = 0
-         self.__velocidad = 0
-         self.__red = 0
-         self.__green = 0
-         self.__blue = 0
+    def __init__(self, id=0, origen_x=0, origen_y=0, destino_x=0, destino_y=0, velocidad=0, red = 0, green=0, blue=0):
+         self.__id  = id
+         self.__origen_x = origen_x
+         self.__origen_y = origen_y
+         self.__destino_x = destino_x
+         self.__destino_y = destino_y
+         self.__velocidad = velocidad
+         self.__red = red
+         self.__green = green
+         self.__blue = blue
          self.__distancia = distancia_euclidiana(self.__origen_x, self.__destino_x, self.__origen_y, self.__destino_y)
 
     def __str__(self):
@@ -28,5 +28,5 @@ class Particula :
             "Distancia: " + str(self.__distancia)
         )
 
-# particula = Particula(1,2,3,4,5,6,7,8,9,10.0)
-# print(particula)
+particula = Particula(1,2,3,4,5,6,7,8,9)
+print(particula)
